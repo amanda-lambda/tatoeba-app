8bfc65977e75cd090f3750323a65b9ded92a2081
Amanda Vu <amanda321@berkeley.edu>
2021-07-10 15:47:34 -0400

    some spinner responses!

--- app/src/main/java/com/example/tatoeba/MainActivity.java
+++ app/src/main/java/com/example/tatoeba/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.tatoeba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,11 +25,41 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         from_spinner.setAdapter(adapter);
+        // Default from language - English
+        from_spinner.setSelection(10);
+        // Spinner selection listener
+        from_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+           @Override
+           public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),
+                "From Spinner : " + parent.getItemAtPosition(position).toString(),
+                Toast.LENGTH_SHORT).show();
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> parent) {
+           }
+        });
 
         // TO SPINNER -----------------
         Spinner to_spinner = (Spinner) findViewById(R.id.to_spinner);
         // Apply the adapter to the spinner
         to_spinner.setAdapter(adapter);
+        // Default to language - Vietnamese
+        to_spinner.setSelection(11);
+        // Spinner selection listener
+        to_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),
+                        "To Spinner : " + parent.getItemAtPosition(position).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 
 }
